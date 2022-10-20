d104227db7641e4a031a878062b440d06115116c
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-20 17:02:22 +0700

    add method input

--- QuickSort2/Program.cs
+++ QuickSort2/Program.cs
@@ -16,6 +16,31 @@ namespace QuickSort2
         // Nummber of elements in array
         private int n;
 
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements");
+            }
+            Console.WriteLine("\n==========================");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("\n==========================");
+
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
 
 
 
