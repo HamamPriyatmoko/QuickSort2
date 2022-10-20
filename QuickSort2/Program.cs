a818af306b90211f04b46ac03599c525f0edd2ca
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-20 17:10:11 +0700

    Add method swap

--- QuickSort2/Program.cs
+++ QuickSort2/Program.cs
@@ -40,7 +40,15 @@ namespace QuickSort2
                 arr[i] = Int32.Parse(s1);
             }
         }
+        //Swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp; 
 
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;  
+        }
 
 
 
