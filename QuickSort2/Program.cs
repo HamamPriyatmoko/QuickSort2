314ada879d72a0a83425c54ab3c654ba1dd9ecc9
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-20 17:28:33 +0700

    add method q_sort

--- QuickSort2/Program.cs
+++ QuickSort2/Program.cs
@@ -49,7 +49,45 @@ namespace QuickSort2
             arr[x] = arr[y];
             arr[y] = temp;  
         }
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
 
+            //Partition the list into two parts:
+            // one containing elements less that or equal to pivot
+            // outher containning elements greather than pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //Search for an element greater than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //Search forn an element less than or equal to pivot
+                if (i < j) // if the greater element is on the left of the element
+                {
+                    //Swap the element at index i with the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+                // sort the list on the left of the pivot using quick sort
+                q_sort(low, i - 1);
+
+                //Sort the list on the right of pivot using quick sort
+                q_sort(j + 1, high);
+            }
+        }
 
 
         static void Main(string[] args)
@@ -60,3 +98,18 @@ namespace QuickSort2
        
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
