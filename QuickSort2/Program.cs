86faaf6206323612631c0384c54c437c4a7debbc
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2022-10-20 16:49:27 +0700

    Add project files.

---
+++ QuickSort2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort2
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // Nummber of elements in array
+        private int n;
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+    
+       
+    
+}
